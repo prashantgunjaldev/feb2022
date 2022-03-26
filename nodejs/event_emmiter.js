0911b3d956fc9bfc752f8a6d9caab321d40b1750
Prashant Gunjal <prashantgunjal.dev@gmail.com>
2022-03-26 11:56:46 +0530

    Student CRUD

--- nodejs/event_emmiter.js
+++ nodejs/event_emmiter.js
@@ -2,11 +2,11 @@ let events = require('events');
 
 let eventEmitter = new events.EventEmitter();
 
-eventEmitter.on('call',()=>{
+eventEmitter.on('call',(data)=>{
     console.log("Someone is calling");
 });
 
 
-eventEmitter.emit('call');
+eventEmitter.emit('call',{data:"test"});
 eventEmitter.emit('call');
 eventEmitter.emit('call');
